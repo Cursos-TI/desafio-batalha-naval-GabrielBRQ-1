bf02a8fa3142a16c36f2908906c52c09b4a2fdeb
Gabriel Valle <123693276+GabrielBRQ@users.noreply.github.com>
2026-05-12 18:23:01 +0000

    add nivel aventureiro

--- batalhaNaval.c
+++ batalhaNaval.c
@@ -10,6 +10,38 @@ int main()
     // Sugestão: Declare uma matriz bidimensional para representar o tabuleiro (Ex: int tabuleiro[5][5];).
     // Sugestão: Posicione dois navios no tabuleiro, um verticalmente e outro horizontalmente.
     // Sugestão: Utilize `printf` para exibir as coordenadas de cada parte dos navios.
+    // int tabuleiro[10][10];
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     for (int j = 0; j < 10; j++)
+    //     {
+    //         tabuleiro[i][j] = 0;
+    //     }
+    //     printf("\n");
+    // }
+
+    // tabuleiro[0][0] = 3;
+    // tabuleiro[0][1] = 3;
+    // tabuleiro[0][2] = 3;
+
+    // tabuleiro[9][9] = 3;
+    // tabuleiro[8][9] = 3;
+    // tabuleiro[7][9] = 3;
+
+    // for (int i = 0; i < 10; i++)
+    // {
+    //     for (int j = 0; j < 10; j++)
+    //     {
+    //         printf("%d  ", tabuleiro[i][j]);
+    //     }
+    //     printf("\n");
+    // }
+
+    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
+    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
+    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
+    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
     int tabuleiro[10][10];
 
     for (int i = 0; i < 10; i++)
@@ -25,6 +57,14 @@ int main()
     tabuleiro[0][1] = 3;
     tabuleiro[0][2] = 3;
 
+    tabuleiro[2][0] = 3;
+    tabuleiro[3][1] = 3;
+    tabuleiro[4][2] = 3;
+
+    tabuleiro[7][4] = 3;
+    tabuleiro[8][3] = 3;
+    tabuleiro[9][2] = 3;
+
     tabuleiro[9][9] = 3;
     tabuleiro[8][9] = 3;
     tabuleiro[7][9] = 3;
@@ -37,11 +77,6 @@ int main()
         }
         printf("\n");
     }
-
-    // Nível Aventureiro - Expansão do Tabuleiro e Posicionamento Diagonal
-    // Sugestão: Expanda o tabuleiro para uma matriz 10x10.
-    // Sugestão: Posicione quatro navios no tabuleiro, incluindo dois na diagonal.
-    // Sugestão: Exiba o tabuleiro completo no console, mostrando 0 para posições vazias e 3 para posições ocupadas.
 
     // Nível Mestre - Habilidades Especiais com Matrizes
     // Sugestão: Crie matrizes para representar habilidades especiais como cone, cruz, e octaedro.
